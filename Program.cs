5c722b0d54f4ae85125b1a0ec11af5cab639d37d
holzjuann <juanhg.holz@gmail.com>
2024-11-19 13:49:07 -0300

    Adicionando injeções de dependências e serviços

--- Program.cs
+++ Program.cs
@@ -1,17 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using minimal_api.src.Domain.DTOs;
+using minimal_api.src.Domain.Interfaces;
+using minimal_api.src.Domain.Services;
+using minimal_api.src.Infrastructure.DB;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IAdmService, AdmService>();
+
+builder.Services.AddDbContext<Context>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionDb"));
+});
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (LoginDTO loginDTO) => {
-    if (loginDTO.Email == "admin@teste.com" && loginDTO.Password == "123456")
+app.MapPost("/login", ([FromBody]LoginDTO loginDTO, IAdmService admService) => {
+    if (admService.Login(loginDTO) != null)
     {
         return Results.Ok("Login Aprovado!");
     } else
     {
-        return Results.NotFound();
+        return Results.Unauthorized();
     }
 });
 
